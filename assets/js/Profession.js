8695b86cb5b9be230de694fd65124b606857236d
Stefan Ledin <Stefan@Stefan-Ledin.local>
2016-02-04 16:35:16 +0100

    personalbilder

--- assets/js/Profession.js
+++ assets/js/Profession.js
@@ -2,7 +2,7 @@ var Profession = function (index, data) {
   this.index = index;  
   this.profession = data.profession;
   this.description = data.description;
-  this.image = null;
+  this.image = data.image;
 
   this.element = this.render();
 };
@@ -11,6 +11,7 @@ Profession.prototype.render = function() {
   var element = document.createElement('div');
   element.innerHTML = '<h3>'+this.profession+'</h3>';
   element.style.left = (this.index * 180) + 'px';
+  element.style.backgroundImage = 'url("' + this.image + '")';
   element.setAttribute('data-pair', this.profession);
   element.classList.add('profession');
   return element;
